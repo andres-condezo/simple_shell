e98a38dbe8954a61e5ed7c9d3d25911da7463117
joseheco <jferhc@gmail.com>
2021-04-10 19:11:34 -0500

    Fix betty

--- straux.c
+++ straux.c
@@ -25,16 +25,17 @@ int _strlen(char *s)
 int _strcspn(char *s, const char *delim)
 {
 	int counter = 0, i;
+
 	while (*s)
-	{
-	for (i = 0; delim[i] != '\0'; i++)
-	{
-		if (*s == delim[i])
-			return (counter);
-	}
-	counter++;
-	s++;
-	}
+		{
+		for (i = 0; delim[i] != '\0'; i++)
+		{
+			if (*s == delim[i])
+				return (counter);
+		}
+			counter++;
+			s++;
+		}
 	return (counter);
 }
 
@@ -48,47 +49,49 @@ int _strcspn(char *s, const char *delim)
 int _strspn(char *s, const char *delim)
 {
 	int i;
+
 	for (i = 0; (s[i] == delim[i]) && (s[i] != '\0' && delim[i] != '\0'); i++)
 	{
-	;
+		;
 	}
 	return (i);
 }
 
 /**
-  * _strtok_r - Find the first token in a string until the dilimiter
-  *             and save the rest of the string in save_str.
-  * @s: String.
-  * @delim: Delimiter.
-  * @save_str: The rest of the string.
-  * Return: First word tokenized.
-  */
+* _strtok_r - Find the first token in a string until the dilimiter
+*             and save the rest of the string in save_str.
+* @s: String.
+* @delim: Delimiter.
+* @save_str: The rest of the string.
+* Return: First word tokenized.
+*/
 
 char *_strtok_r(char *s, const char *delim, char **save_str)
 {
-    char *end;
-    if (s == NULL)
-        s = *save_str;
-    if (*s == '\0')
-    {
-        *save_str = s;
-        return (NULL);
-    }
-    s += _strspn(s, delim);
-    if (*s == '\0')
-    {
-        *save_str = s;
-        return (NULL);
-    }
-    end = s + _strcspn(s, delim);
-    if (*end == '\0')
-    {
-        *save_str = end;
-        return (s);
-    }
-    *end = '\0';
-    *save_str = end + 1;
-    return (s);
+	char *end;
+
+	if (s == NULL)
+		s = *save_str;
+	if (*s == '\0')
+	{
+		*save_str = s;
+		return (NULL);
+	}
+	s += _strspn(s, delim);
+	if (*s == '\0')
+	{
+		*save_str = s;
+		return (NULL);
+	}
+	end = s + _strcspn(s, delim);
+	if (*end == '\0')
+	{
+		*save_str = end;
+		return (s);
+	}
+	*end = '\0';
+	*save_str = end + 1;
+	return (s);
 }
 
 /**
@@ -99,7 +102,9 @@ char *_strtok_r(char *s, const char *delim, char **save_str)
 */
 
 char *_strtok(char *s, const char *delim)
+
 {
-    static char *olds;
-    return (_strtok_r(s, delim, &olds));
+	static char *olds;
+
+	return (_strtok_r(s, delim, &olds));
 }
