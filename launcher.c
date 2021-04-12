1c062608d379dca6fa2b2698d567a1864ac48d23
joseheco <jferhc@gmail.com>
2021-04-12 14:55:34 -0500

    working

--- launcher.c
+++ launcher.c
@@ -6,36 +6,42 @@
   */
 int _launch(char **args)
 {
-    int status, pid;
-    int flag = 0;
-    args = _check_path(args, &flag);
-    if (args == NULL)
-        return (1);
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execve(args[0], args, environ) == -1)
-        {
-            perror("hsh");
-        }
-        if (flag == 1)
-            free(args[0]);
-        return (-1);
-    }
-    else if (pid < 0)
-    {
-        perror("hsh");
-        if (flag == 1)
-            free(args[0]);
-        return (-1);
-    }
-    else
-    {
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    if (flag == 1)
-        free(args[0]);
-    return (1);
+	int status, pid;
+	int flag = 0;
+
+	args = _check_path(args, &flag);
+
+	if (args == NULL)
+		return (1);
+	pid = fork();
+
+	if (pid == 0)
+	{
+	if (execve(args[0], args, environ) == -1)
+		{
+		perror("hsh");
+		}
+
+		if (flag == 1)
+			free(args[0]);
+		return (-1);
+	}
+
+	else if (pid < 0)
+
+	{
+		perror("hsh");
+		if (flag == 1)
+			free(args[0]);
+		return (-1);
+	}
+	else
+	{
+		do {
+		waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	if (flag == 1)
+		free(args[0]);
+	return (1);
 }
