fa61a24b8c7ed4f7833f9c2d72739455c093d663
adrs condezo <ra.condezomonge@gmail.com>
2021-04-12 16:37:57 -0500

    add auxiliar prototypes

--- shell.h
+++ shell.h
@@ -7,6 +7,29 @@
 #include <signal.h>
 #include <stddef.h>
 
+/* Environmental variables */
+extern char **environ;
+
+/* Function declarations (prototypes for for builtin shell commands */
+int hsh_exit(char **args, char *input);
+int _cd(char **args, __attribute__((unused)) char *input);
+/**
+  * _help - Help function
+  * @args: List of arguments passed from parsing.
+  * @input: Input line for free.
+  * Return: 1 if works.
+  */
+int _help(__attribute__((unused)) char **args,
+	__attribute__((unused)) char *input);
+/**
+  * _env - Env function
+  * @args: List of arguments passed from parsing.
+  * @input: Input line for free.
+  * Return: 1 if works.
+  */
+int _env(__attribute__((unused)) char **args,
+	__attribute__((unused)) char *input);
+int _setenv(char *name, char *value);
 
 /* Prototypes for main functions*/
 void _loop(void);
@@ -22,21 +45,21 @@ char *_getenv(const char *name);
 ssize_t _getline(char **b, size_t *bufsize, FILE *stream);
 
 int _getc(void);
-
 char *_strtok_r(char *s, const char *delim, char **save_str);
-
 int _strcspn(char *s, const char *delim);
-
 int _strspn(char *s, const char *delim);
-
 char *_strtok(char *s, const char *delim);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strncmp(const char *s1, const char *s2, int n);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 int alloc_error(char *buffer);
-
 int _strlen(char *s);
-
 int _putchar(char c);
+
+
+
 
 #endif
