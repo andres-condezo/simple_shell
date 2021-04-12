0971da366f118ea828c2ce1a077ccb84a17f8e8a
joseheco <jferhc@gmail.com>
2021-04-12 14:41:48 -0500

    Second part

--- executer.c
+++ executer.c
@@ -11,3 +11,24 @@ int (*builtin_func[])(char **args, char *input) = {
     &_env,
 };
 
+/**
+  * _execute - Execute builtin process.
+  * @args: List of arguments passed from parsing.
+  * @input: Input line for free.
+  * Return: _launch(args).
+  */
+int _execute(char **args, char *input)
+{
+    char *builtin_str[] = {"cd", "help", "exit", "env"};
+    int i;
+    if (args[0] == NULL)
+        return (1);
+    if (_strcmp(args[0], "setenv") == 0)
+        return (_setenv(args[1], args[2]));
+    for (i = 0; i < 4; i++)
+    {
+        if (_strcmp(args[0], builtin_str[i]) == 0)
+            return ((*builtin_func[i])(args, input));
+    }
+    return (_launch(args));
+}
